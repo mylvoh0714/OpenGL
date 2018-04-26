9173e77e8a3452258e947e275b1f19a4cc0b8c20
hyuntaek <oh0714@hanyang.ac.kr>
2018-04-26 18:35:28 +0900

    first opengl program

---
+++ First_OpenGL_Program.py
@@ -0,0 +1,33 @@
+import glfw
+from OpenGL.GL import *
+
+def render():
+    pass
+
+def main():
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(640,480,"Hello World",None,None)
+    if not window:
+        glfw.terminate()
+        return
+
+    # Make the window's context current
+    glfw.make_context_current(window)
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Poll events
+        glfw.poll_events()
+
+        # Render here, e.g using pyOpenGL
+        render()
+
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
